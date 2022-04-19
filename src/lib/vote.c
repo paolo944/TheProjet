f4d1f2b9363cd35731c5af612a150b9fe409e983
PaulinDeuxTrois <95508677+PaulinDeuxTrois@users.noreply.github.com>
2022-04-19 13:22:08 +0200

    oui

--- src/lib/vote.c
+++ src/lib/vote.c
@@ -9,23 +9,15 @@ void submit_vote(Protected* p){
     fclose(F);
     free(pstr);    
 }
-/*
-CellProtected* vote=create_cell_protected(p);
-    Block* bloc = malloc(sizeof(Block));
-    bloc->author=p->pKey;
-    bloc->previous_hash=NULL;
-    bloc->nonce=0;
-    bloc->hash=miaou256(protected_to_str("123"));//123 pour que ça ne passe pas le premier test
-    bloc->votes=vote;                           //compute_proof_of_work mettra tout seul la bonne valeur hash
-    compute_proof_of_work(bloc,5);
-    ecrire_block(bloc,"Pending_votes.txt");
-*/
+
 
 void create_block(CellTree* tree, Key* author, int d) {
     Block* Pending_block=malloc(sizeof(Block));
     Pending_block->votes=read_protected("Pending_votes.txt");
     remove("Pending_votes.txt");
     Pending_block->author=author;
+    Pending_block->previous_hash=last_node(tree)->block->hash;
+    //peut-être il faut rajouter a la fin du tree
     compute_proof_of_work(Pending_block,d);
     ecrire_block(Pending_block,"Pending_bloc.txt");
 }
